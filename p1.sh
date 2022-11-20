a2493f6540879cdadeb48aece2bd6705e01aa808
Anshika Soni <91690050+SoniAnshika@users.noreply.github.com>
2022-11-21 01:25:53 +0530

    Add files via upload

---
+++ p1.sh
@@ -0,0 +1,11 @@
+echo "Enter your Name: "
+read name
+echo "Enter your Program name: "
+read prog
+echo "Enter your Enrollment Number: "
+read enroll
+clear
+echo "Details you entered are :"
+echo Name: $name
+echo Program Name: $prog
+echo Enrollment Number: $enroll 
